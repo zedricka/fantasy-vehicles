f50e8fe66b2415195c460749d7f49d1a5cbfb139
zedricka <zedrick.ara@gmail.com>
2017-08-30 10:24:21 -1000

    Fixed

--- fantasy-vehicles.js
+++ fantasy-vehicles.js
@@ -68,6 +68,35 @@ class SuperCar extends Vehicle{
 
 }
 
+class Motorcycle extends Vehicle{
+  constructor(make, color, type, topSpeed, cost){
+    super(make, color, type);
+    this._topSpeed = topSpeed;
+    this._cost = cost;
+  }
+
+  get bikeDetails(){
+    return "Your vehicle is a " + this._type + ", made by " + this._make + " and is " + this._color + ' with top speed of ' + this._topSpeed + " and a cost of " + this._cost + "."
+  }
+
+  set bikeSpeed(topSpeed){
+    if(typeof(topSpeed) === "string"){
+        this._topSpeed = "9001";
+    }else{
+      throw new TypeError("Not a string baka");
+    }
+  }
+
+  set bikeCost(cost){
+    if(typeof(cost) === "string"){
+        this._cost = "420";
+    }else{
+      throw new TypeError("Not a string baka");
+    }
+  }
+
+}
+
 // let newVehicle = new Vehicle();
 let newSuperCar = new SuperCar("BMW", "Truck", "White", "9001", "420");
 
