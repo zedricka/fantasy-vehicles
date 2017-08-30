cff130f284bbfe967c4777de11099b8dc80561b1
zedricka <zedrick.ara@gmail.com>
2017-08-30 09:43:42 -1000

    Fixed

--- fantasy-vehicles.js
+++ fantasy-vehicles.js
@@ -7,6 +7,10 @@ class Vehicle {
     this._type = type;
   }
 
+  get vehicleDetails(){
+    return "Your vehicle is a " + this.type + ", made by " + this.make + " and is " + this.color + ".";
+  }
+
   set vehicleMake(make){
     this._make = "BMW";
   }
@@ -19,10 +23,8 @@ class Vehicle {
     this._color = "White";
   }
 
-  get vehicleDetails(){
-    return "Your vehicle is a " + this.type + ", made by " + this.make + " and is " + this.color + ".";
-  }
 }
+
 class SuperCar extends Vehicle{
   constructor(make, color, type, topSpeed, cost){
     super(make, color, type);
